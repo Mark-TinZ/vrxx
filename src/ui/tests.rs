06891617b15ead30d84c285e722792ac7b076d55
Mark-TinZ <91620987+Mark-TinZ@users.noreply.github.com>
2026-04-24 06:35:54 +0000

    Cache D-Bus connection for improved backend performance

--- src/ui/tests.rs
+++ src/ui/tests.rs
@@ -1,6 +1,9 @@
 #[cfg(test)]
 mod tests {
     use gtk::{gio, glib};
+    use glib::subclass::types::ObjectSubclassIsExt;
+    use gtk::prelude::TextViewExt;
+    use gtk::prelude::TextBufferExt;
 
     // Need to initialize GTK before creating widgets.
     fn init_gtk() {
